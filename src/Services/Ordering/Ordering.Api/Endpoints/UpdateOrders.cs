339aa5c70b86d4cb355468411043138f22b166d7
Johna210 <solomonjohna21@gmail.com>
2024-12-04 12:26:17 +0300

    Added Api endpoints

---
+++ src/Services/Ordering/Ordering.Api/Endpoints/UpdateOrders.cs
@@ -0,0 +1,30 @@
+﻿using Ordering.Application.orders.Commands.UpdateOrder;
+
+namespace Ordering.Api.Endpoints;
+
+public record UpdateOrderRequest(OrderDto Order);
+
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrders : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateOrderCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<UpdateOrderResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("UpdateOrder")
+            .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Update an order")
+            .WithDescription("Update an order in the system");
+    }
+}
